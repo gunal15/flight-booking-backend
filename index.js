58897642697db49764afe75854d09b9eaf1dfa02
gunal15 <gunalnandakumar@gmail.com>
2023-03-31 15:09:42 +0530

    add

--- index.js
+++ index.js
@@ -9,23 +9,6 @@ const dateRoute = require("./routes/dateChecker");
 const ticketRoute = require("./routes/ticket-booking");
 const mailRoute = require("./routes/mail");
 
-var instance = new Razorpay({
-  key_id: 'rzp_test_XpMWpnZvs94XYZ',
-  key_secret: 'gWwfvb4Xo9R2e0JtWlstwV8c',
-});
-
-const Razorpay = require('razorpay');
-var instance = new Razorpay({ key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_SECRET' })
-
-var options = {
-  amount: 200,  // amount in the smallest currency unit
-  currency: "INR",
-  receipt: "order_rcptid_11"
-};
-instance.orders.create(options, function(err, order) {
-  console.log(order);
-});
-
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successful"))
