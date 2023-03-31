94d13b4e1e050f4dde6f792f155f7fe271cf03ba
gunal15 <gunalnandakumar@gmail.com>
2023-03-31 14:55:43 +0530

    razorpay integration

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ var instance = new Razorpay({
   key_secret: 'gWwfvb4Xo9R2e0JtWlstwV8c',
 });
 
+const Razorpay = require('razorpay');
+var instance = new Razorpay({ key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_SECRET' })
+
+var options = {
+  amount: 200,  // amount in the smallest currency unit
+  currency: "INR",
+  receipt: "order_rcptid_11"
+};
+instance.orders.create(options, function(err, order) {
+  console.log(order);
+});
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successful"))
--- routes/ticket-booking.js
+++ routes/ticket-booking.js
@@ -2,55 +2,44 @@ const router = require("express").Router();
 const { default: mongoose } = require("mongoose");
 const Ticket = require("../model/Ticket");
 const { verifyToken } = require("./verifytoken");
+const Razorpay = require('razorpay');
 
-router.post("/book-ticket/:userid",verifyToken, async (request, response) => {
+// Create a new Razorpay instance with your API key and secret
+const razorpay = new Razorpay({
+  key_id: 'rzp_test_XpMWpnZvs94XYZ',
+  key_secret: 'gWwfvb4Xo9R2e0JtWlstwV8c',
+});
+
+router.post("/book-ticket/:userid", verifyToken, async (request, response) => {
   const newTicket = new Ticket({
     date: request.body.date,
     from: request.body.from,
     to: request.body.to,
     price: request.body.price,
     airline: request.body.airline,
-
     userid: request.params.userid,
     food: request.body.food,
   });
 
   try {
     const savedTicket = await newTicket.save();
+
+    // Create a new payment order with the ticket price
+    const order = await razorpay.orders.create({
+      amount: savedTicket.price * 100, // amount in paise (i.e. 500 rupees)
+      currency: 'INR',
+      receipt: savedTicket._id.toString(),
+      payment_capture: 1,
+    });
+
     response.status(200).json({
       message: "Ticket booked!",
       savedTicket,
+      order_id: order.id, // Send the order ID to the front-end
     });
   } catch (error) {
     response.status(500).json(error);
   }
 });
 
-router.get("/ticket/:userid/:ticketid", verifyToken, async (request, response) => {
-  try {
-    let ticket = await Ticket.findById(request.params.ticketid);
-    if (ticket) {
-      response.status(200).json({ message: "Ticket Found", ticket });
-    } else {
-      response.status(400).json({ message: "Ticket Not Found" });
-    }
-  } catch (error) {
-    response.status(500).json(error);
-  }
-});
-
-router.get("/allticket/:userid",verifyToken, async (request, response) => {
-  try {
-    let tickets = await Ticket.find({
-      userid: mongoose.Types.ObjectId(request.params.userid),
-    });
-    if (tickets) {
-      response.status(200).json({ message: "Ticket Found", tickets });
-    } else {
-      response.status(400).json({ message: "Ticket Not Found" });
-    }
-  } catch (error) {
-    response.status(500).json(error);
-  }
-});
 module.exports = router;
